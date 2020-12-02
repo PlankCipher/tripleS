9ce01b821300c8d22b336ea8bbd5ff6c993d9c7a
PlankCipher <plankcipher1@protonmail.com>
2020-12-02 20:52:21 +0200

    Add sounds directory option

--- tripleS.py
+++ tripleS.py
@@ -2,6 +2,8 @@ import os
 import pyxhook
 import random
 import argparse
+from os import path, listdir
+import re
 
 continuous = False
 last_key = None
@@ -10,20 +12,54 @@ parser = argparse.ArgumentParser(
     description='Keyboard switches sound simulator.')
 parser.add_argument('-v', '--volume', type=float, default=1,
                     help='Volume of switch sound (float)')
-
+parser.add_argument('-s', '--sounds', required=True, type=str,
+                    help='Path to directory cotaining switches sounds. Have a look at https://github.com/PlankCipher/tripleS/#Sounds for more info')
 
 args = parser.parse_args()
+
+enter_path = ''
+space_path = ''
+others_paths = []
+
+if not path.isdir(args.sounds):
+    print('Couldn\'t parse sounds')
+    exit()
+else:
+    files = listdir(args.sounds)
+
+    if len(files) > 0:
+        enter_re = re.compile('^enter\.(mp3|wav)$')
+        space_re = re.compile('^space\.(mp3|wav)$')
+        other_re = re.compile('^.+\.(mp3|wav)$')
+
+        for file in files:
+            if re.match(enter_re, file) != None:
+                enter_path = file
+            elif re.match(space_re, file) != None:
+                space_path = file
+            elif re.match(space_re, file) == None and re.match(enter_re, file) == None and re.match(other_re, file) != None:
+                others_paths.append(file)
+    else:
+        print('Couldn\'t parse sounds')
+        exit()
+
+    if not enter_path or not space_path or len(others_paths) == 0:
+        print('Couldn\'t parse sounds')
+        exit()
 
 
 def KeyDownHandle(event):
     global continuous, last_key
 
-    sound = 'sounds/modelm/'
+    sound = args.sounds
 
     if event.Key == 'space':
-        sound += 'space.wav'
+        sound = path.join(sound, space_path)
+    elif event.Key == 'Return':
+        sound = path.join(sound, enter_path)
     else:
-        sound += f'{random.randint(1, 11)}.wav'
+        rand_index = random.randint(0, len(others_paths) - 1)
+        sound = path.join(sound, others_paths[rand_index])
 
     if not continuous or event.Key != last_key:
         last_key = event.Key
